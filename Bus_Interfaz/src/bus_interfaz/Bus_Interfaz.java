3141f7fc233488654d1a733a163a99fbe8032ff7
Manuel Hernandez <manuel.hernandez28@outlook.com>
2023-04-16 14:40:56 +0200

    iniciando repocitorio

---
+++ Bus_Interfaz/src/bus_interfaz/Bus_Interfaz.java
@@ -0,0 +1,13 @@
+package bus_interfaz;
+
+
+public class Bus_Interfaz {
+
+    public static void main(String[] args) {
+        
+        formulario frm = new formulario();
+        frm.show();
+        
+    }
+    
+}
